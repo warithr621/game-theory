58bea9f18f10a5656ea274f9c4f749101df01376
Warith Rahman <warithr21@gmail.com>
2025-03-21 22:20:17 -0500

    reorganized viewport
    me when I debug for 1e9+7 hours because I don't have a .env

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,17 @@
-import type { Metadata } from 'next';
+import type { Metadata, Viewport } from 'next';
 import './styles/globals.css';
 
 export const metadata: Metadata = {
 	title: 'Game Theory',
-	description: 'An online card game',
-	viewport: {
-		width: 'device-width',
-		initialScale: 1,
-		maximumScale: 1,
-		userScalable: false
-	}
+	description: 'An online card game'
 };
+
+export const viewport: Viewport = {
+	width: 'device-width',
+	initialScale: 1,
+	maximumScale: 1,
+	userScalable: false
+}
 
 export default function RootLayout({
 	children,
